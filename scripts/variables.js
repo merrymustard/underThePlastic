ca24522bf3d2c6a410f5f56593601b5f483d0e51
merrymustard <merryymustard@gmail.com>
2019-11-08 11:14:23 -0600

    variables files

---
+++ scripts/variables.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+const bubbless = new Bubbles();
+const ocean = new Ocean();
+const lifeCrabby = new LivesCrab();
+const lifeSharky = new LivesShark();
+const shark = new Shark();
+const crab = new Crabby();
+const trash = new Trash();
+const fish = new FishFood();
+const iconShark = new IconShark(28,56,canvas.width-244,20);
+const iconCrab = new IconCrab(28,56,20,20);
+const shipCan = [];
